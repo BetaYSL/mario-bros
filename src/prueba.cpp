5dbd4c639fb387f81693575c9c36253df242d21d
BetaYSL <a23310288@live.ceti.mx>
2024-11-02 17:41:28 +0000

    Examen

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -8,36 +8,57 @@
 using namespace std;
 using namespace ftxui;
 
+int posicionX(int posicion, int tiempo, int velocidad)
+{
+    return posicion + velocidad * tiempo;
+}
+
+int posicionY(int posicion, int tiempo, int velocidad)
+{
+    return posicion + velocidad * tiempo;
+}
+
 int main(int argc, char const *argv[])
 {
-    std::string reset_position;
     int frame = 0;
-    auto can = Canvas(250,700);
-    can.DrawPointCircle(10,10,10);
-    can.DrawPointEllipse(10,10,10,10);
-     can.DrawPointEllipse(10,15,45,15);
-    auto ColorSupport = vbox({
-                            Terminal::ColorSupport() >= Terminal::Color::Palette16
-                                ? text(" 16 color palette support : Yes")
-                                : text(" 16 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::Palette256
-                                ? text("256 color palette support : Yes")
-                                : text("256 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::TrueColor
-                                ? text("       True color support : Yes")
-                                : text("       True color support : No"),
-                        });
+    int velocidad_x = 2;
+    int velocidad_y = 1;
+    int posx = 10;
+    int posy = 10;
+
+    int ancho_canvas = 100;
+    int alto_canvas = 60;
+
     while (true)
     {
+
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        Element personaje = spinner(20, frame);
-        Element lienzo = bgcolor(Color::LightSalmon3Bis, border(vbox(ColorSupport,personaje,canvas(&can))));
-        Render(pantalla, lienzo);
-        std::cout << reset_position;
+        auto can = Canvas(ancho_canvas, alto_canvas);
+
+
+        can.DrawPointEllipseFilled(posx, posy, 18, 8, Color::White);
+        can.DrawPointEllipseFilled(posx, posy, 14, 6.5, Color::Aquamarine1);
+        can.DrawPointCircle(posx, posy, 2, Color::Black);
+
+        auto lienzo = canvas(&can);
+        auto layout = vbox({lienzo});
+
+        Render(pantalla, layout);
         pantalla.Print();
-        reset_position = pantalla.ResetPosition(true);
-        this_thread::sleep_for(0.1s);
-        frame++;
+
+        posx += velocidad_x;
+        posy += velocidad_y;
+
+        if (posx + 10 >= ancho_canvas || posx -10 <= 0)
+        {
+            velocidad_x = -velocidad_x;
+        }
+        if (posy + 8 >= alto_canvas || posy -8 <= 0)
+        {
+            velocidad_y = -velocidad_y;
+        }
+                this_thread::sleep_for(0.08s);
+
     }
 
     return 0;
